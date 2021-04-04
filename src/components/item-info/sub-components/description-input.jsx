aa14d8251cc5725cacbaa76723e20a0500228ae1
msanand <shanmugan.murugesan@anywhere.co>
2021-04-04 17:36:25 +0530

    css for description modal

--- src/components/item-info/sub-components/description-input.jsx
+++ src/components/item-info/sub-components/description-input.jsx
@@ -4,6 +4,7 @@ const DescriptionInput = ({value, onChange}) => {
     return (
         <li className = "description input-wrapper">
             <input placeholder='Product Name'  type="text" value={value} onChange={onChange}/>
+            <a className="write-review">Write Description</a>
         </li>
     )
 }
